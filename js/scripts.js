02baaf65c530e5dd11a9d9e7b6905ae32459d51b
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-16 16:17:49 -0700

    complete back-end portion of assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-function passwordParser(numbers) {
-  if(parseInt(numbers) === 1) {
-  return "beep";
-  } else if(parseInt(numbers) === 2) {
-    return "boop";
-  } else if(parseInt(numbers) === 3) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
+let responseArray = [];
+let callArray = [];
+let inputtedNumber;
+
+function inputToArray(inputtedNumber) {
+  parseInt(inputtedNumber);
+  for (let i = 1; i <= inputtedNumber; i++)
+  callArray.push(i.toString());
+  console.log(callArray);
+}
+function passwordParser() {
+  callArray.forEach(function(element) {
+    if (element.includes('3')) {
+      responseArray.push("I'm sorry, Dave. I'm afraid I can't do that");
+    } else if (element.includes('2')) {
+      responseArray.push('boop');
+    } else if (element[0] === '1') {
+      responseArray.push('beep');
+    } else {
+      responseArray.push(element)
+    }
+  });
+  console.log(responseArray.join(' '));
 }
