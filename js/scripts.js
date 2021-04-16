3d86302257563165871af153be4faeaf2f689b45
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-16 12:33:12 -0700

    add else ifs to passwordParser function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 function passwordParser(numbers) {
   if(parseInt(numbers) === 1) {
   return "beep";
+  } else if(parseInt(numbers) === 2) {
+    return "boop";
+  } else if(parseInt(numbers) === 3) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
   }
 }
