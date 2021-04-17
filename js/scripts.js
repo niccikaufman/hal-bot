f9c712d6b9ce8e2aba9fb0cea750b7829a5c6ddc
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-17 16:01:20 -0700

    final product, fingers crossed

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ let callArray = [];
 let inputtedNumber;
 
 function inputToArray(inputtedNumber) {
-  inputtedNumber = document.getElementById('password-input');
   console.log(inputtedNumber);
   for (let i = 1; i <= inputtedNumber; i++)
   callArray.push(i.toString());
@@ -16,7 +15,7 @@ function passwordParser(callArray) {
       responseArray.push("I'm sorry, Dave. I'm afraid I can't do that");
     } else if (element.includes('2')) {
       responseArray.push('boop');
-    } else if (element[0] === '1') {
+    } else if (element.includes('1')) {
       responseArray.push('beep');
     } else {
       responseArray.push(element)
@@ -25,15 +24,39 @@ function passwordParser(callArray) {
   console.log(responseArray.join(' '));
 }
 
+function clearAll() {
+  responseArray = [];
+  callArray = [];
+  console.log(responseArray);
+}
+
 //front end
 document.getElementById('landing-button').addEventListener('click', () => {
   $('#landing-page').toggle();
   $('#main-page').show();
+  $('#hal-dialog-box').hide();
+  $('#password').show();
 });
 
 $('#password-submit').on('click', function(event) {
   event.preventDefault();
+  $('#hal-dialog-box').show();
+  $('.password').toggle();
+  inputtedNumber = document.getElementById("password-input").value;
   inputToArray(inputtedNumber);
   passwordParser(callArray);
-  $("#hal-dialog").text(responseArray.join(' '));
+  document.getElementById("hal-dialog").innerHTML = responseArray.join(' ');
 })
+
+$('#reset').on('click', function(event) {
+  event.preventDefault();
+  document.getElementById("hal-dialog").innerHTML = '';
+  document.getElementById('password-input').value = '';
+  $('#landing-page').toggle();
+  $('#main-page').hide();
+  $('.password').toggle();
+  clearAll();
+});
+
+
+
